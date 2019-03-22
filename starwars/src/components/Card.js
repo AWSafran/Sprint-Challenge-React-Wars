ecc1956ebf3a27ae0869cafbb1fcf1e3b106d617
AWSafran <andrewsafran@gmail.com>
2019-03-22 11:52:23 -0400

    props being passed and accessed correctly in Card.js

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Card = props =>{
-    return <div className='card'>Card Here</div>
+    console.log(props);
+    return <div className='card'>
+        <h2>Name: {props.card.name}</h2>
+        <p>Birth Year: {props.card.birth_year}</p>
+        {/* <a href={props.card.homeworld} target="_blank">Home World</a> */}
+        <p>Mass: {props.card.mass} kg</p>
+    </div>
 }
 
 export default Card;
